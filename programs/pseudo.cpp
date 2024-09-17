1232b34b7bcdd759df5811fe7a75d2f06c54703f
T0mTG <dogiaminh0612@gmail.com>
2024-09-16 19:02:05 -0500

    first commit

---
+++ programs/pseudo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll n,a[1000009],res=0;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout<<"Welcome to the magical Age calculator\n"<<"Please enter ur age"<<endl;
+    cin>>n;
+    cout<<"Your age is "<<n;
+}
