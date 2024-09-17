2212b0d64ee262464aa3ebe8541a9c29e86e925b
T0mTG <dogiaminh0612@gmail.com>
2024-09-16 19:05:59 -0500

    second

--- programs/pseudo.cpp
+++ programs/pseudo.cpp
@@ -2,11 +2,19 @@
 #define ll long long
 using namespace std;
 ll n,a[1000009],res=0;
+void chek()
+{
+    for(int i=2;i<=sqrt(10000009);i++)
+    {
+        
+    }
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout<<"Welcome to the magical Age calculator\n"<<"Please enter ur age"<<endl;
     cin>>n;
-    cout<<"Your age is "<<n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+
+
 }
